e8e8a86d7a411ca96dbb194733129a20b9f6d9aa
CBruxter <118524813+CBruxter@users.noreply.github.com>
2022-12-10 17:17:09 +0100

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,37 @@
+/********************************************/
+/* Reseña de Epic Mickey                    */
+/* Hojas de estilo para autores             */
+/* Autor: Miguel Ángel Ávila Rosas          */
+/* Version: 1.0                             */
+/* Creado el: 21 de octubre de 2022         */
+/* Actualizado el: 8 de diciembre de 2022   */
+/********************************************/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
